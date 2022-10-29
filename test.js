ffc480a56a7a2d1d83686583f7698f5ee76392cb
davet7 <45505926+davet7@users.noreply.github.com>
2022-10-29 08:48:06 -0500

    base app

---
+++ test.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import moduleName from 'module'
+
+import React from 'react'
+
+export default function test() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+
+function App() {
+  return <div className="test">Hello World</div>;
+}
+
+export default App;
